3d708b0ab66130fff66886243bdfdfe26c007d79
Nick Garrett <garrett.nick.96@gmail.com>
2020-06-06 16:54:08 -0700

    Username to be specified as command line argument

--- spotipyservices.py
+++ spotipyservices.py
@@ -3,16 +3,21 @@ import sys
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+if (len(sys.argv) != 2):
+    print("Usage: python spotipyservices.py [your-spotify-username]")
+    print("Specify your Spotify username")
+    sys.exit()
+
 client_id = os.getenv('SPOTIPY_CLIENT_ID')
 client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
-username = os.getenv('SPOTIPY_USERNAME')
 redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
 scope = "playlist-modify-public"
+username = sys.argv[1]
 
 token = spotipy.util.prompt_for_user_token(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
 
 if token:
     sp = spotipy.Spotify(auth=token)
-    print("Authorized")
+    print("Authorized", username)
 else:
-    print("Failed to authorize")
+    print("Failed to authorize", username)
