3c933c4cf5d72d64d681e26d1ba24469d37a109f
Nick Garrett <garrett.nick.96@gmail.com>
2020-06-06 19:32:21 -0700

    Added method to list user playlists

--- spotipyservices.py
+++ spotipyservices.py
@@ -1,13 +1,13 @@
 import os
 import sys
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 
 if (len(sys.argv) != 2):
     print("Usage: python spotipyservices.py [your-spotify-username]")
-    print("Specify your Spotify username")
+    print("Specify your Spotify username.")
     sys.exit()
 
+# Auth
 client_id = os.getenv('SPOTIPY_CLIENT_ID')
 client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
 redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
@@ -15,9 +15,22 @@ scope = "playlist-modify-public"
 username = sys.argv[1]
 
 token = spotipy.util.prompt_for_user_token(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+sp = spotipy.Spotify(auth=token)
 
+# Methods
+def list_my_playlists():
+    playlists = sp.user_playlists(username)
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+        if playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+
+# Script
 if token:
-    sp = spotipy.Spotify(auth=token)
     print("Authorized", username)
+    list_my_playlists()
 else:
     print("Failed to authorize", username)
