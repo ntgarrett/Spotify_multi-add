17740e8d0b4b93c0c1edbf26668914bcd62ed897
Nick Garrett <garrett.nick.96@gmail.com>
2020-11-19 19:32:04 -0600

    indent consistency

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 if (len(sys.argv) > 1):
-    username = sys.argv[1]
+  username = sys.argv[1]
 else:
-    print("Usage: python app.py [your-spotify-username]")
-    print("Specify your Spotify username.")
-    sys.exit()
+  print("Usage: python app.py [your-spotify-username]")
+  print("Specify your Spotify username.")
+  sys.exit()
 
 # Auth
 client_id = os.getenv('SPOTIPY_CLIENT_ID')
@@ -21,51 +21,51 @@ sp = spotipy.Spotify(auth=token)
 
 # Spotipy methods
 def list_my_playlists():
-    playlists = sp.user_playlists(username)
-    results = []
-    while playlists:
-        for i, playlist in enumerate(playlists['items']):
-            if (playlist['owner']['id'] == username):
-                results.append({
-                    "id": playlist['id'],
-                    "name": playlist['name'] 
-                })
-        if playlists['next']:
-            playlists = sp.next(playlists)
-        else:
-            playlists = None
-        return jsonify(results)
+  playlists = sp.user_playlists(username)
+  results = []
+  while playlists:
+    for i, playlist in enumerate(playlists['items']):
+      if (playlist['owner']['id'] == username):
+          results.append({
+            "id": playlist['id'],
+            "name": playlist['name'] 
+          })
+      if playlists['next']:
+        playlists = sp.next(playlists)
+      else:
+        playlists = None
+      return jsonify(results)
 
 def find_song(key):
-    return sp.search(q=key,type='track',market="from_token")
+  return sp.search(q=key,type='track',market="from_token")
 
 def add_track_to_playlists(track, selectedPlaylists):
-    for playlist in selectedPlaylists:
-        sp.user_playlist_add_tracks(username,playlist,track)
+  for playlist in selectedPlaylists:
+    sp.user_playlist_add_tracks(username,playlist,track)
 
 app = Flask(__name__)
 CORS(app, resources=r'/api/*')
 
 @app.route('/api')
 def index():
-    return "Authorized {}".format(username)
+  return "Authorized {}".format(username)
 
 @app.route('/api/playlists')
 def playlists():
-    return list_my_playlists()
+  return list_my_playlists()
 
 @app.route('/api/search')
 def search():
-    key = request.args.get('key')
-    return find_song(key)
+  key = request.args.get('key')
+  return find_song(key)
 
 @app.route('/api/addtrack', methods=['POST'])
 def addtrack():
-    req_data = request.get_json()
-    track_id = (req_data['Track_ID'])
-    playlist_ids = req_data["Playlist_IDs"]
-    add_track_to_playlists([track_id], playlist_ids)
-    return "Added " + str(track_id) + " to playlists: " + str(playlist_ids)
+  req_data = request.get_json()
+  track_id = (req_data['Track_ID'])
+  playlist_ids = req_data["Playlist_IDs"]
+  add_track_to_playlists([track_id], playlist_ids)
+  return "Added " + str(track_id) + " to playlists: " + str(playlist_ids)
     
 if __name__ == '__main__':
-    app.run()
+  app.run()
--- my-app/src/Playlists.js
+++ my-app/src/Playlists.js
@@ -1,60 +1,59 @@
 import React from 'react';
-import axios from 'axios'; 
+import axios from 'axios';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import CheckBox from '@material-ui/core/Checkbox';
 
-
 export default class Playlists extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            playlists: [],
-            selectedPlaylists: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      playlists: [],
+      selectedPlaylists: []
+    };
+  }
 
-    handleChange(e, playlist) {
-        const checkedList = this.state.selectedPlaylists;
-        
-        if (e.target.checked) {
-            checkedList.push(playlist);
-        } else {
-            const index = checkedList.indexOf(playlist);
-            checkedList.splice(index,1);
-        }
-        this.setState({ selectedPlaylists: checkedList});
-    }
+  handleChange(e, playlist) {
+    const checkedList = this.state.selectedPlaylists;
 
-    componentDidMount() {
-        axios.get('http://127.0.0.1:5000/api/playlists')
-            .then((response) => {
-                this.setState({playlists: response.data});
-            })
-            .catch((error) => console.log(error));
+    if (e.target.checked) {
+      checkedList.push(playlist);
+    } else {
+      const index = checkedList.indexOf(playlist);
+      checkedList.splice(index, 1);
     }
+    this.setState({ selectedPlaylists: checkedList });
+  }
 
-    render() { 
-        const plistItems = this.state.playlists.map((playlist) =>
-            <ListItem key={playlist.id}>
-                <ListItemText primary={playlist.name}/>
-                <ListItemIcon>
-                    <CheckBox 
-                        onChange={(e) => {
-                            this.handleChange(e, playlist);
-                            console.log(this.state.selectedPlaylists);
-                        }}
-                    />
-                </ListItemIcon>
-            </ListItem>
-            
-        );     
-        return (
-            <List>
-                {plistItems}
-            </List>
-        );
-    }
+  componentDidMount() {
+    axios.get('http://127.0.0.1:5000/api/playlists')
+      .then((response) => {
+        this.setState({ playlists: response.data });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  render() {
+    const plistItems = this.state.playlists.map((playlist) =>
+      <ListItem key={playlist.id}>
+        <ListItemText primary={playlist.name} />
+        <ListItemIcon>
+          <CheckBox
+            onChange={(e) => {
+              this.handleChange(e, playlist);
+              console.log(this.state.selectedPlaylists);
+            }}
+          />
+        </ListItemIcon>
+      </ListItem>
+
+    );
+    return (
+      <List>
+        {plistItems}
+      </List>
+    );
+  }
 }
