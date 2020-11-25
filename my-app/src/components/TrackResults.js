b64d94ffedca941ae194671d7a11206f014ad2ff
Nick Garrett <garrett.nick.96@gmail.com>
2020-11-25 03:30:28 -0600

    Lists one track per row

--- my-app/src/components/TrackResults.js
+++ my-app/src/components/TrackResults.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import TrackContainer from '../containers/TrackContainer';
 import List from '@material-ui/core/List';
+import { Grid } from '@material-ui/core/';
 
 const TrackResults = (props) => {
-  const {tracks, setSelectedTrack, selectedTrack} = props;
-  
+  const { tracks, setSelectedTrack, selectedTrack } = props;
+
   return (
     <List>
       {tracks.map((track) => {
         return (
-          <TrackContainer 
-            key={track.id}
-            track={track}
-            setSelectedTrack={setSelectedTrack}
-            selectedTrack={selectedTrack}
-          />
+          <Grid item xs={12} sm={6} key={track.id}>
+            <TrackContainer
+              track={track}
+              setSelectedTrack={setSelectedTrack}
+            />
+          </Grid>
         );
       })}
     </List>
