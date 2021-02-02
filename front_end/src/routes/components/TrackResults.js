e673e0525504bdc1def24312eaf369b15788610d
Nick Garrett <garrett.nick.96@gmail.com>
2021-02-02 17:59:39 -0600

    Moves loading state handling into component

--- front_end/src/routes/components/TrackResults.js
+++ front_end/src/routes/components/TrackResults.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import TrackContainer from '../containers/TrackContainer';
 import List from '@material-ui/core/List';
-import { Grid } from '@material-ui/core/';
+import { Grid, Container, CircularProgress} from '@material-ui/core/';
 
 const TrackResults = (props) => {
-  const { tracks, setSelectedTrack, selectedTrack } = props;
+  const { tracks, setSelectedTrack, selectedTrack, theme, loading } = props;
 
   return (
-    <div>
-      <List style={{ visibility: tracks.length > 0 ? "visible" : "hidden" }}>
+    loading ? 
+    <Container
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <CircularProgress color="secondary" />
+    </Container>
+    : (<List style={{ visibility: tracks.length > 0 ? "visible" : "hidden" }}>
         {tracks.map((track) => {
           return (
             <Grid item key={track.id}>
@@ -16,13 +27,13 @@ const TrackResults = (props) => {
                 track={track}
                 setSelectedTrack={setSelectedTrack}
                 selectedTrack={selectedTrack}
+                theme={theme}
               />
             </Grid>
           );
         })}
-      </List>
-    </div>
+      </List>)
   );
-}
+};
 
 export default TrackResults;
--- front_end/src/routes/containers/TrackResultsContainer.js
+++ front_end/src/routes/containers/TrackResultsContainer.js
@@ -2,15 +2,17 @@ import React from 'react';
 import TrackResults from '../components/TrackResults';
 
 const TrackResultsContainer = (props) => {
-  const {tracks, setSelectedTrack, selectedTrack} = props;
+  const {tracks, setSelectedTrack, selectedTrack, theme, loading} = props;
 
   return (
     <TrackResults
       tracks={tracks}
+      theme={theme}
       setSelectedTrack={setSelectedTrack}
       selectedTrack={selectedTrack}
+      loading={loading}
     />
   );
-}
+};
 
 export default TrackResultsContainer;
