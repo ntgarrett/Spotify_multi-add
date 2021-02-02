ef48ad6b7fa520e00b6150cf6572ee25216a72b1
Nick Garrett <garrett.nick.96@gmail.com>
2021-02-02 17:57:17 -0600

    Adjusts styling

--- front_end/src/routes/401.js
+++ front_end/src/routes/401.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import { AuthContext } from '../utils/Context';
 
 const styles = () => ({
   root: {
-    minWidth: "100vw",
-    minHeight: "100vh",
-    background: "linear-gradient(#000000, #1db950)",
+    flexGrow: 1,
   },
   title: {
     display: "flex",
     justifyContent: "center",
     fontFamily: "Didact Gothic",
-    fontSize: "4em",
-    color: "white",
     paddingTop: "15vh",
   },
   button: {
@@ -30,24 +28,36 @@ const styles = () => ({
 });
 
 const UnauthorizedPage = (props) => {
-  const { history, classes } = props;
+  const { history, classes, logoutPressed } = props;
+  const { setAuthenticated } = useContext(AuthContext);
+
+  useEffect(() => {
+    setAuthenticated(false);
+  }, [setAuthenticated]);
+
   return (
+    logoutPressed ?
+    <Redirect to="/"/>
+    :
     <div className={classes.root}>
       <Typography
         className={classes.title}
+        variant="h1"
       >
-        401: Unauthorized Access
-      </Typography>
+        401: Unauthorized
+        </Typography>
       <Button
         className={classes.button}
         size="large"
         variant="contained"
-        onClick={() => {history.push("/")}}
+        onClick={() => {
+          history.push("/");
+        }}
       >
-        Go to Login
-      </Button>
+        Return to Login
+        </Button>
     </div>
   );
-}
+};
 
 export default withStyles(styles)(UnauthorizedPage);
--- front_end/src/routes/404.js
+++ front_end/src/routes/404.js
@@ -1,20 +1,17 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
 const styles = () => ({
   root: {
-    minWidth: "100vw",
-    minHeight: "100vh",
-    background: "linear-gradient(#000000, #1db950)",
+    flexGrow: 1,
   },
   title: {
     display: "flex",
     justifyContent: "center",
     fontFamily: "Didact Gothic",
-    fontSize: "4em",
-    color: "white",
     paddingTop: "15vh",
   },
   button: {
@@ -30,14 +27,19 @@ const styles = () => ({
 });
 
 const PageNotFound = (props) => {
-  const { history, classes } = props;
+  const { history, classes, logoutPressed } = props;
+
   return (
+    logoutPressed ?
+    <Redirect to="/"/>
+    :
     <div className={classes.root}>
       <Typography
         className={classes.title}
+        variant="h1"
       >
         404: Page Not Found
-      </Typography>
+        </Typography>
       <Button
         className={classes.button}
         size="large"
@@ -47,9 +49,9 @@ const PageNotFound = (props) => {
         }}
       >
         Return to Application
-      </Button>
+        </Button>
     </div>
   );
-}
+};
 
 export default withStyles(styles)(PageNotFound);
