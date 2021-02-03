8ff5d91bd5193348effe8d3611938f75f8dd5c82
Nick Garrett <garrett.nick.96@gmail.com>
2021-02-02 18:00:37 -0600

    Adds button to launch track in spotify and adjusts styling

--- front_end/src/routes/components/Track.js
+++ front_end/src/routes/components/Track.js
@@ -3,48 +3,89 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = (theme) => ({
-  button: {
-    width: "100%",
-  },
+const styles = () => ({
   trackinfo: {
     marginLeft: "1.5em",
+    marginRight: "1em",
   },
-  primary: {
+  title: {
     fontFamily: "Didact Gothic",
     maxWidth: "100%",
   },
-  secondary: {
+  artist: {
     fontFamily: "Didact Gothic",
-    fontSize: "1em",
-    fontStyle: "italic",
+  },
+  playbutton: {
+    marginRight: "0.5em",
   },
 });
 
 const Track = (props) => {
-  const { classes, track } = props;
+  const { classes, track, handleClick, selectedTrack, theme } = props;
+
+  function colorSelector(track, selectedTrack) {
+    return (track === selectedTrack)
+      ? (theme ? "#b6b6b6" : "#595959")
+      : "inherit";
+  }
+
+  const preventDefault = (event) => event.preventDefault;
 
   return (
     <div>
       <ButtonBase
-        className={classes.button}
         key={track.id}
-        onClick={e => { props.handleClick(e, track) }}
+        onClick={e => { handleClick(e, track) }}
+        style={{
+          width: "100%",
+          backgroundColor: colorSelector(track.id, selectedTrack.id),
+        }}
       >
         <ListItem>
           <Avatar variant="square" alt="album-cover" src={track.album.images[0].url} />
-          <ListItemText 
+          <ListItemText
             className={classes.trackinfo}
-            primary={<Typography noWrap className={classes.primary}>{track.name}</Typography>}
-            secondary={<Typography noWrap className={classes.secondary}>{(track.artists).map((artist) => artist.name).join(', ')}</Typography>}
+            primary={
+              <Typography
+                className={classes.title}
+                noWrap
+                variant="body1"
+              >
+                {track.name}
+              </Typography>
+            }
+            secondary={
+              <Typography
+                className={classes.artist}
+                noWrap
+                variant="body2"
+              >
+                {(track.artists).map((artist) => artist.name).join(', ')}
+              </Typography>
+            }
           />
+          <Tooltip 
+            title="Open in Spotify App"
+            TransitionComponent={({ children }) => children}    
+          >
+            <Link
+              href={track.uri}
+              onClick={preventDefault}
+              className={classes.playbutton}
+            >
+              <PlayCircleFilledIcon color="secondary"/>
+            </Link>
+          </Tooltip>
         </ListItem>
       </ButtonBase>
     </div>
   );
-}
+};
 
 export default withStyles(styles)(Track);
--- front_end/src/routes/containers/TrackContainer.js
+++ front_end/src/routes/containers/TrackContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Track from '../components/Track';
 
 const TrackContainer = (props) => {
-  const {track, setSelectedTrack, selectedTrack} = props;
+  const {track, setSelectedTrack, selectedTrack, theme} = props;
 
   const handleClick = (e,track) => {
     if (track === selectedTrack) {
@@ -16,8 +16,10 @@ const TrackContainer = (props) => {
     <Track 
       track={track}
       handleClick={handleClick}
+      selectedTrack={selectedTrack}
+      theme={theme}
     />
   );
-}
+};
 
 export default TrackContainer;
